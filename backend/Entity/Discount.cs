4d916de28543605ed498777369c12f98f0b06665
Pury <dongson@gmail.com>
2023-09-11 14:44:57 +0700

    haidang

--- backend/Entity/Discount.cs
+++ backend/Entity/Discount.cs
@@ -5,11 +5,9 @@ namespace backend.Entity
     public class Discount : BaseCreateDate
     {
         public int Id { get; set; }
-        public int discount { get; set; }
-        public string? type { get; set; }
-        public bool isActive { get; set; } = true;
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public Tour? Tour { get; set; }
+        public string Discount1 { get; set; }
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public string? Description { get; set; }
     }
 }
