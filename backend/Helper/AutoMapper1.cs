4741d7f4a8a699b259fa588c1f1eaacdb9b1b7f2
Em yeu anh Hung nhieu lam <muxica2411@gmail.com>
2023-09-13 21:59:17 +0700

    Update-template

---
+++ backend/Helper/AutoMapper1.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace backend.Helper
+{
+    public class AutoMapper1 : Profile
+    {
+        public AutoMapper1()
+        {
+            //CreateMap<Category, CategoryDto>().ReverseMap();
+
+        }
+
+    }
+}
