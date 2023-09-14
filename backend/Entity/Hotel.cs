9c6065e57499fcd0a4518f014a8d81ae90658a3b
Đăng Tây <dang.nt.2052@aptechlearning.edu.vn>
2023-09-14 16:42:00 +0700

    hai dang

---
+++ backend/Entity/Hotel.cs
@@ -0,0 +1,23 @@
+﻿namespace backend.Entity
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public int Rate { get; set; }
+        public int LocatinId { get; set; }
+
+        public string? Description { get; set; }
+        public int ImageDetail { get; set; }
+        public string? Address { get; set; }
+        public string? Image { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public string? CreateBy { get; set; }
+        public string? UpdateBy { get; set; }
+        public int PhoneNumber { get; set; }
+        public bool IsActive { get; set; } = true;
+
+    }
+}
