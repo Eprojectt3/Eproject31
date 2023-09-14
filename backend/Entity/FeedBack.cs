a505704fa7217e87a163e7f9f85fd46f4bedd862
Đăng Tây <dang.nt.2052@aptechlearning.edu.vn>
2023-09-14 16:42:00 +0700

    hai dang

--- backend/Entity/FeedBack.cs
+++ backend/Entity/FeedBack.cs
@@ -1,13 +1,10 @@
-using webapi.Base;
-
 namespace backend.Entity
 {
-    public class FeedBack : BaseCreateDate
+    public class FeedBack
     {
         public int Id { get; set; }
-        public string Title { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
-        public int? Phone { get; set; }
+        public int Phone { get; set; }
     }
 }
