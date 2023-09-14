2d7bcc881ffa69278e49c391bd22cb47326d556b
Đăng Tây <dang.nt.2052@aptechlearning.edu.vn>
2023-09-14 16:42:00 +0700

    hai dang

---
+++ backend/Entity/Discount.cs
@@ -0,0 +1,16 @@
+﻿using webapi.Base;
+
+namespace backend.Entity
+{
+    public class Discount:BaseCreateDate
+    {
+        public int Id { get; set; }
+        public int discount { get; set; }
+        public string? type { get; set; }
+        public bool isActive { get; set; } = true;
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Tour? Tour { get; set; }
+        
+    }
+}
