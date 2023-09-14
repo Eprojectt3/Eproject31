a67023361ec07959bc981de4762f323aafe61f1e
Em yeu anh Hung nhieu lam <muxica2411@gmail.com>
2023-09-14 15:40:37 +0700

    commit ENtity

--- backend/Entity/Restaurant.cs
+++ backend/Entity/Restaurant.cs
@@ -5,16 +5,13 @@ namespace backend.Entity
     public class Restaurant : BaseCreateDate
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public double Price { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
         public int Ratings { get; set; }
-        public string? Description { get; set; }
-        public string? Address { get; set; }
-        public string? Avatar { get; set; }
-        public string? Image_detail { get; set; }
+        public string State { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string Image { get; set; }
         public int PhoneNumbber { get; set; }
-        public int LocationId { get; set; }
-        public Location1? Location { get; set; }
-        public string? link { get; set; }
     }
 }
