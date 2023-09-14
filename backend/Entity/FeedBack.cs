cb5285112e5d7f3563d27f756dd567c98f18db3b
Tran Thanh Dat <dat.tt.2041>
2023-09-14 22:43:49 +0700

    Create Entity in C#

--- backend/Entity/FeedBack.cs
+++ backend/Entity/FeedBack.cs
@@ -1,10 +1,13 @@
+using webapi.Base;
+
 namespace backend.Entity
 {
-    public class FeedBack
+    public class FeedBack : BaseCreateDate
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
-        public int Phone { get; set; }
+        public int? Phone { get; set; }
     }
 }
--- backend/Entity/Restaurant.cs
+++ backend/Entity/Restaurant.cs
@@ -5,7 +5,7 @@ namespace backend.Entity
     public class Restaurant : BaseCreateDate
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public double Price { get; set; }
         public int Ratings { get; set; }
         public string Description { get; set; }
