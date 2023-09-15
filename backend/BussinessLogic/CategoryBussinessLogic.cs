f384f08678c7b055ee74050515a413ba1d86528b
dangdz2810 <nonome2810@gmail.com>
2023-09-15 12:32:52 +0700

    Create Entitycrud category

--- backend/BussinessLogic/CategoryBussinessLogic.cs
+++ backend/BussinessLogic/CategoryBussinessLogic.cs
@@ -1,36 +1,105 @@
-﻿using backend.Entity;
+﻿using backend.Dao.Specification;
+using backend.Entity;
+using backend.Exceptions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Web.Http.ModelBinding;
+using webapi.Dao.Specification;
 using webapi.Dao.UnitofWork;
 
 namespace backend.BussinessLogic
 {
     public class CategoryBussinessLogic
     {
-        public IUnitofWork unitofWork { get; set; }
+        public IUnitofWork unitofWork;
         public CategoryBussinessLogic(IUnitofWork _unitofWork) {
             unitofWork = _unitofWork;
         }
 
-    
-
+        //list category
         public async Task<IReadOnlyList<Category>> SelectAllCategory()
         {
             var data = await unitofWork.Repository<Category>().GetAllAsync();
             return data;
         }
+
+        //create category
         public async Task Create(Category category)
         {
+            if (category is null)
+            {
+                throw new NotFoundExceptions("Cattegory not found");
+            }
+
+            if (await IsCategoryNameDuplicate(category.Name))
+            {
+                throw new BadRequestExceptions("Category Name is exist.");
+            }
+            
+
             await unitofWork.Repository<Category>().AddAsync(category);
-            await unitofWork.Complete();
+            var check = await unitofWork.Complete();
+            if (check < 1)
+            {
+                throw new BadRequestExceptions("chua dc thuc thi");
+            }
         }
-        public async Task Update(Category category)
+
+        //update category
+        public async Task Update(int id,Category category)
         {
-            await unitofWork.Repository<Category>().Update(category);
-            await unitofWork.Complete();
+            if (category is null)
+            {
+                throw new NotFoundExceptions("not found");
+            }
+            
+            var existingCategory = await unitofWork.Repository<Category>().GetByIdAsync(id);
+            
+            if (existingCategory is null)
+            {
+               throw new NotFoundExceptions("not found");
+            }
+            existingCategory.UpdateDate = category.UpdateDate ;
+            existingCategory.CreateDate = category.CreateDate;
+            existingCategory.UpdateBy = category.UpdateBy ;
+            existingCategory.CreateBy = category.CreateBy ;
+            existingCategory.Name = category.Name ;
+            existingCategory.IsActive = category.IsActive ;
+
+            await unitofWork.Repository<Category>().Update(existingCategory);
+            var check = await unitofWork.Complete();
+            if (check < 1)
+            {
+                throw new BadRequestExceptions("chua dc thuc thi");
+            }
         }
-        public async Task Delete(Category category)
+
+        //delete category
+        public async Task Delete(int id)
         {
-            await unitofWork.Repository<Category>().Delete(category);
-            await unitofWork.Complete();
+            
+            var existingCategory = await unitofWork.Repository<Category>().GetByIdAsync(id);
+            if (existingCategory == null) { 
+                throw new NotFoundExceptions("not found");
+            }
+            await unitofWork.Repository<Category>().Delete(existingCategory);
+            var check = await unitofWork.Complete();
+            if (check < 1)
+            {
+                throw new BadRequestExceptions("chua dc thuc thi");
+            }
+        }
+
+        //duplicate name
+        private async Task<bool> IsCategoryNameDuplicate(string categoryName)
+        {
+            // Chuyển tên category thành chữ thường để so sánh không phân biệt chữ hoa/chữ thường
+            categoryName = categoryName.ToLower();
+           
+            // Sử dụng GetEntityWithSpecAsync để kiểm tra trùng lặp
+            var duplicateCategory = await unitofWork.Repository<Category>()
+                .GetEntityWithSpecAsync(new CategoryByNameSpecification(categoryName));
+
+            return duplicateCategory != null;
         }
     }
 }
--- backend/Controllers/CategoryController.cs
+++ backend/Controllers/CategoryController.cs
@@ -9,13 +9,13 @@ namespace backend.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        public CategoryBussinessLogic categoryBussinessLogic { get; set; }
+        public CategoryBussinessLogic categoryBussinessLogic;
         public CategoryController(CategoryBussinessLogic categoryBussiness)
         {
             categoryBussinessLogic = categoryBussiness;
         }
 
-        // list all category
+        // execute list all category
         [HttpGet]
         public async Task<ActionResult> ListCategory()
         {
@@ -27,90 +27,34 @@ namespace backend.Controllers
             return Ok(output);
         }
 
-        //add new category
+        //execute add new category
         [HttpPost]
-      
+
         public async Task<IActionResult> Add(Category category)
         {
-            if (category is null)
-            {
 
-                return Ok(NotFound());
-            }
-            if (!ModelState.IsValid)
-            {
-
-                return Ok(ModelState);
-            }
-            bool isDuplicate = await IsCategoryNameDuplicate(category.Name);
-            if (isDuplicate)
-            {
-                ModelState.AddModelError("DuplicateName", "CategoryName has been exist.");
-                return BadRequest(ModelState);
-            }
             await categoryBussinessLogic.Create(category);
 
             return Ok(category);
         }
 
-        //update category
+        //execute update category
         [HttpPost]
-        public async Task<IActionResult> Update(int id,Category category)
+        public async Task<IActionResult> Update(int id, Category category)
         {
-            if (category is null)
-            {
 
-                return Ok(NotFound());
-            }
-            var existingCategory = await categoryBussinessLogic.SelectAllCategory();
-            existingCategory.FirstOrDefault(cat => cat.Id == id);
-            if (existingCategory == null)
-            {
-                return NotFound();
-            }
-            if (!ModelState.IsValid)
-            {
-
-                return Ok(ModelState);
-            }
-            bool isDuplicate = await IsCategoryNameDuplicate(category.Name);
-            if (isDuplicate)
-            {
-                ModelState.AddModelError("DuplicateName", "CategoryName has been exist.");
-                return BadRequest(ModelState);
-            }
-            await categoryBussinessLogic.Update(category);
+            await categoryBussinessLogic.Update(id, category);
             return Ok(category);
         }
 
-        //delete category
+        //execute delete category
         [HttpDelete]
-        public async Task<IActionResult> Delete(int id )
-        {
-            
-            var existingCategory = await categoryBussinessLogic.SelectAllCategory();
-            Category category = existingCategory.FirstOrDefault(cat => cat.Id == id);
-            if (existingCategory == null)
-            {
-                return NotFound();
-            }
-            if (!ModelState.IsValid)
-            {
-
-                return Ok(ModelState);
-            }
-            await categoryBussinessLogic.Delete(category);
-            return Ok(category);
+        public async Task<IActionResult> Delete(int id)
+        {           
+            await categoryBussinessLogic.Delete(id);
+            return Ok();
         }
 
-        //duplicate name 
-        private async Task<bool> IsCategoryNameDuplicate(string categoryName)
-        {
-            // Truy vấn danh sách tất cả danh mục hiện có trong ứng dụng
-            var allCategories = await categoryBussinessLogic.SelectAllCategory();
-
-            // Kiểm tra xem tên danh mục đã tồn tại trong danh sách hay chưa
-            return allCategories.Any(c => c.Name.Equals(categoryName, StringComparison.OrdinalIgnoreCase));
-        }
+        
     }
 }
