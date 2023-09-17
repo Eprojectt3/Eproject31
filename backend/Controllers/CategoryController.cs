3cc8d31514a1f405215728e002783b0fd07d2dfa
Tran Thanh Dat <dat.tt.2041>
2023-09-17 11:18:51 +0700

    Create Redis

--- backend/Controllers/CategoryController.cs
+++ backend/Controllers/CategoryController.cs
@@ -1,7 +1,11 @@
 ﻿using backend.BussinessLogic;
+using backend.Dao.Repository;
 using backend.Entity;
+using backend.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using System;
 
 namespace backend.Controllers
 {
@@ -10,15 +14,17 @@ namespace backend.Controllers
     public class CategoryController : ControllerBase
     {
         public CategoryBussinessLogic categoryBussinessLogic;
-
-        public CategoryController(CategoryBussinessLogic categoryBussiness)
+        private readonly IResponseCacheService responseCacheService;
+        public CategoryController(CategoryBussinessLogic categoryBussiness, IResponseCacheService responseCache)
         {
             categoryBussinessLogic = categoryBussiness;
+            responseCacheService = responseCache;
         }
 
         // execute list all category
         [HttpGet]
-        public async Task<ActionResult> ListCategory()
+        [Cache(1400)]
+        public async Task<ActionResult> ListCategory(int id = 1, string hehe = "abc")
         {
             var output = await categoryBussinessLogic.SelectAllCategory();
             if (output == null)
@@ -34,6 +40,8 @@ namespace backend.Controllers
         {
             await categoryBussinessLogic.Create(category);
 
+            var api = "/api/Category/ListCategory*";
+            responseCacheService.RemoveCache(api);
             return Ok(category);
         }
 
