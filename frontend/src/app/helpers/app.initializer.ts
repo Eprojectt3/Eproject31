c650b358e18ad92123fdebe2f14529d345d72afa
Pury <dongson2001@gmail.com>
2023-09-19 13:52:01 +0700

    son fe fix

--- frontend/src/app/helpers/app.initializer.ts
+++
@@ -1,6 +0,0 @@
-import { catchError, finalize, of } from 'rxjs';
-import { AuthService } from '../services/auth.service';
-
-export function appInitializer(authService: AuthService) {
-  return () => authService.refreshToken().pipe(catchError(() => of()));
-}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
+
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
