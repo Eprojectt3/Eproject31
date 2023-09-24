8049a257e0a4c53cde90db762cae2bc163a84224
Đăng Tây <dang.nt.2052@aptechlearning.edu.vn>
2023-09-24 14:54:09 +0700

    DangTay/Admin

---
+++ frontend/src/app/pages/admin/admin-itinerary/create-itineraries/create-itineraries.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-itineraries',
+  templateUrl: './create-itineraries.component.html',
+  styleUrls: ['./create-itineraries.component.scss']
+})
+export class CreateItinerariesComponent {
+
+}
