a00cb03a6c21621f80b59651cd266d7071e09be4
Đăng Tây <dang.nt.2052@aptechlearning.edu.vn>
2023-09-24 14:54:09 +0700

    DangTay_TourList_26/9

--- frontend/src/app/pages/infomation/tours/tours-list/tours-list.component.ts
+++ frontend/src/app/pages/infomation/tours/tours-list/tours-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Tour, tours } from 'src/app/models/tour';
+import { TitleService } from 'src/app/services/title.service';
 
 @Component({
   selector: 'app-tours-list',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./tours-list.component.scss']
 })
 export class ToursListComponent {
+  tours: Tour[] = tours;
+  selected = 'none';
 
+  constructor(private titleService: TitleService) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitleValue('Tour List');
+  }
 }
