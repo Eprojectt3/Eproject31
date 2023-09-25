9e06d8c68fe3603e91d0266a2568e113d335db99
Pury <dongson@gmail.com>
2023-09-25 12:11:50 +0700

    son fe tour list and detail 25-09-2023

--- frontend/src/app/pages/infomation/tours/tours-list/tours-list.component.ts
+++ frontend/src/app/pages/infomation/tours/tours-list/tours-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TitleService } from 'src/app/services/title.service';
 
 @Component({
   selector: 'app-tours-list',
   templateUrl: './tours-list.component.html',
-  styleUrls: ['./tours-list.component.scss']
+  styleUrls: ['./tours-list.component.scss'],
 })
-export class ToursListComponent {
+export class ToursListComponent implements OnInit {
+  constructor(private titleService: TitleService) {}
 
+  ngOnInit(): void {
+    this.titleService.setTitleValue('Tours list');
+  }
 }
