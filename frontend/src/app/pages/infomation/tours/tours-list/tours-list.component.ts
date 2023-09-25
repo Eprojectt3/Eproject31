f9492e9ae0adaef451553cadd58e00a9d6718b7e
Pury <dongson@gmail.com>
2023-09-25 12:11:50 +0700

    son fe tour list and detail 26-09-2023

--- frontend/src/app/pages/infomation/tours/tours-list/tours-list.component.ts
+++ frontend/src/app/pages/infomation/tours/tours-list/tours-list.component.ts
@@ -12,6 +12,7 @@ export class ToursListComponent implements OnInit {
   filters: Filter[] = filter;
   isGridView: boolean = true;
   isLineView: boolean = false;
+  tours: Tour[] = tours;
 
   constructor(private titleService: TitleService) {}
 
