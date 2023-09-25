7d8375a7549fcb9d87fc01c598ef631443fe0076
Pury <dongson@gmail.com>
2023-09-25 12:11:50 +0700

    son fe tour list and detail 26-09-2023

--- frontend/src/app/pages/infomation/tours/tours-list/tours-list.component.ts
+++ frontend/src/app/pages/infomation/tours/tours-list/tours-list.component.ts
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
 import { Tour, tours } from 'src/app/models/tour';
-=======
 import { Component, OnInit } from '@angular/core';
 import { Filter, filter } from 'src/app/models/filter.model';
->>>>>>> 7c8fd51 (son fe tour list and detail 26-09-2023)
+
 import { TitleService } from 'src/app/services/title.service';
 
 @Component({
@@ -12,23 +9,14 @@ import { TitleService } from 'src/app/services/title.service';
   templateUrl: './tours-list.component.html',
   styleUrls: ['./tours-list.component.scss'],
 })
-<<<<<<< HEAD
-export class ToursListComponent {
-  tours: Tour[] = tours;
-  selected = 'none';
-=======
 export class ToursListComponent implements OnInit {
   filters: Filter[] = filter;
   isGridView: boolean = true;
   isLineView: boolean = false;
->>>>>>> 7c8fd51 (son fe tour list and detail 26-09-2023)
 
   constructor(private titleService: TitleService) {}
 
   ngOnInit(): void {
-<<<<<<< HEAD
-    this.titleService.setTitleValue('Tour List');
-=======
     this.titleService.setTitleValue('Tours list');
   }
 
@@ -42,6 +30,5 @@ export class ToursListComponent implements OnInit {
     this.isGridView = false;
     this.isLineView = true;
     console.log(this.isGridView);
->>>>>>> 7c8fd51 (son fe tour list and detail 26-09-2023)
   }
 }
