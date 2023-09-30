f972032c1f3b9152c15f2d4e3a37eb0b2c96bdef
Pury <dongson2001@gmail.com>
2023-09-30 13:55:45 +0700

    sondx fe 30-09-2023

--- frontend/src/app/pages/admin/admin-tour/list-tours/list-tours.component.ts
+++ frontend/src/app/pages/admin/admin-tour/list-tours/list-tours.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list-tours',
   templateUrl: './list-tours.component.html',
-  styleUrls: ['./list-tours.component.scss']
+  styleUrls: ['./list-tours.component.scss'],
 })
-export class ListToursComponent {
-
-}
+export class ListToursComponent {}
