d633bd680ec213c063dd03493bed070abae1e381
dangdz2810 <nonome2810@gmail.com>
2023-10-16 22:29:22 +0700

    list

--- frontend/src/app/pages/user/infomation/resorts/resorts-list/resorts-list.component.ts
+++ frontend/src/app/pages/user/infomation/resorts/resorts-list/resorts-list.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Filter, filter } from 'src/app/models/filter.model';
 import { Resort } from 'src/app/models/resort.model';
 
@@ -12,30 +14,47 @@ import { TitleService } from 'src/app/services/title.service';
   templateUrl: './resorts-list.component.html',
   styleUrls: ['./resorts-list.component.scss'],
 })
-export class ResortsListComponent {
+export class ResortsListComponent implements OnInit {
   filters: Filter[] = filter;
-
+  // tours:Tour[]=tours
   resorts!: Resort[];
   selected = 'none';
+  public pageSize: number = 12;
+  public pageIndex: number = 0;
+
+  public pageEvent: PageEvent = new PageEvent();
+  public totalSize: number = 0;
+  public index: number = 1;
+  public dataSource = new MatTableDataSource<Resort>([]);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
     private titleService: TitleService,
-    private resortSecvice: ResortService
+    private resortService: ResortService
   ) {}
 
   ngOnInit(): void {
-    this.titleService.setTitleValue('Resort List');
-    this.resortSecvice.getListResort().subscribe((val: any) => {
-      this.resorts = val;
-      for (let resort of this.resorts) {
-        const firstPeriodIndex = resort.description.indexOf('.');
-        if (firstPeriodIndex !== -1) {
-          resort.description = resort.description.slice(
-            0,
-            firstPeriodIndex + 1
-          );
-        }
-      }
-    });
+    this.titleService.setTitleValue('Hotel List');
+    this.getListResorts();
+
   }
+  public getListResorts = () => {
+    this.resortService
+      .getListResortPagination(this.index, this.pageSize)
+      .subscribe((val: any) => {
+        this.resorts = val.data;
+        console.log( this.resorts);
+        this.totalSize = val.count;
+        this.dataSource = val.data;
+      });
+  };
+
+
+
+  public handlePage = (e: any): any => {
+    this.pageIndex = e.pageIndex;
+    this.pageSize = e.pageSize;
+    this.index = this.pageIndex + 1;
+    this.getListResorts();
+  };
 }
