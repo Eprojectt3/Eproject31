1872f49cd4a46b5491d631f4ac62720828d8c59a
Pury <dongson2001@gmail.com>
2023-10-28 11:21:47 +0700

    sondx add send gmail

--- backend/BussinessLogic/InformationBusinessLogic.cs
+++ backend/BussinessLogic/InformationBusinessLogic.cs
@@ -1,8 +1,11 @@
-﻿using AutoMapper;
+﻿using System.Net;
+using System.Net.Mail;
+using AutoMapper;
 
 using backend.Dao.Specification;
 using backend.Dao.Specification.InformationSpec;
 using backend.Dtos.InformationDtos;
+using backend.Dtos.InformationMailDto;
 using backend.Entity;
 using backend.Exceptions;
 using backend.Helper;
@@ -14,10 +17,31 @@ namespace backend.BussinessLogic
     {
         public IUnitofWork unitofWork;
         public IMapper mapper;
-        public InformationBusinessLogic(IUnitofWork _unitofWork, IMapper mapper)
+        private readonly SmtpClient _smtpClient;
+        private readonly IConfiguration _configuration;
+
+        public InformationBusinessLogic(
+            IUnitofWork _unitofWork,
+            IMapper mapper,
+            IConfiguration configuration
+        )
         {
             unitofWork = _unitofWork;
             this.mapper = mapper;
+            _configuration = configuration;
+
+            _smtpClient = new SmtpClient
+            {
+                Host = _configuration["SmtpConfig:SmtpServer"],
+                Port = int.Parse(_configuration["SmtpConfig:Port"]),
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(
+                    _configuration["SmtpConfig:Username"],
+                    _configuration["SmtpConfig:Password"]
+                ),
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network
+            };
         }
 
         //list information
@@ -28,18 +52,41 @@ namespace backend.BussinessLogic
         }
 
         //create information
-        public async Task Create(Information information)
+        public async Task Create(InformationMailDto information)
         {
+            Information information1 = new Information();
+            information1.Id = information.Id;
+            information1.Location = information.Location;
+            information1.Transportation = information.Transportation;
+            information1.Departure_Date = information.Departure_Date;
+            information1.End_Date = information.End_Date;
+            information1.Destination = information.Destination;
+            information1.Description = information.Description;
+
             if (information is null)
             {
                 throw new NotFoundExceptions("Cattegory not found");
             }
-            await unitofWork.Repository<Information>().AddAsync(information);
+            await unitofWork.Repository<Information>().AddAsync(information1);
             var check = await unitofWork.Complete();
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_configuration["SmtpConfig:Username"]),
+                Subject = "Karnel Travel Guide",
+                Body =
+                    $"Thank you for your trust in using our services. We will contact you in the shortest possible time, usually within 1 day at most",
+                IsBodyHtml = true
+            };
+
             if (check < 1)
             {
                 throw new BadRequestExceptions("chua dc thuc thi");
             }
+
+            mailMessage.To.Add(information.Email);
+
+            _smtpClient.Send(mailMessage);
         }
 
         //update information
@@ -65,7 +112,7 @@ namespace backend.BussinessLogic
             existingInformation.Description = information.Description;
             existingInformation.End_Date = information.End_Date;
             existingInformation.Destination = information.Destination;
-           
+
             await unitofWork.Repository<Information>().Update(existingInformation);
             var check = await unitofWork.Complete();
             if (check < 1)
@@ -77,7 +124,6 @@ namespace backend.BussinessLogic
         //delete information
         public async Task Delete(int id)
         {
-
             var existingInformation = await unitofWork.Repository<Information>().GetByIdAsync(id);
             if (existingInformation == null)
             {
@@ -100,21 +146,36 @@ namespace backend.BussinessLogic
                 throw new NotFoundExceptions("not found");
             }
         }
-        public async Task<Pagination<InformationDto>> SelectAllInformationPagination(SpecParams specParams)
-        {
 
+        public async Task<Pagination<InformationDto>> SelectAllInformationPagination(
+            SpecParams specParams
+        )
+        {
             var spec = new SearchInformationSpec(specParams);
             var resorts = await unitofWork.Repository<Information>().GetAllWithAsync(spec);
 
-            var data = mapper.Map<IReadOnlyList<Information>, IReadOnlyList<InformationDto>>(resorts);
-            var staffPage = data.Skip((specParams.PageIndex - 1) * specParams.PageSize).Take(specParams.PageSize).ToList();
+            var data = mapper.Map<IReadOnlyList<Information>, IReadOnlyList<InformationDto>>(
+                resorts
+            );
+            var staffPage = data.Skip((specParams.PageIndex - 1) * specParams.PageSize)
+                .Take(specParams.PageSize)
+                .ToList();
 
             var countSpec = new SearchInformationSpec(specParams);
             var count = await unitofWork.Repository<Information>().GetCountWithSpecAsync(countSpec);
 
-            var totalPageIndex = count % specParams.PageSize == 0 ? count / specParams.PageSize : (count / specParams.PageSize) + 1;
+            var totalPageIndex =
+                count % specParams.PageSize == 0
+                    ? count / specParams.PageSize
+                    : (count / specParams.PageSize) + 1;
 
-            var pagination = new Pagination<InformationDto>(specParams.PageIndex, specParams.PageSize, staffPage, count, totalPageIndex);
+            var pagination = new Pagination<InformationDto>(
+                specParams.PageIndex,
+                specParams.PageSize,
+                staffPage,
+                count,
+                totalPageIndex
+            );
 
             return pagination;
         }
---
+++ backend/Dtos/InformationDtos/InformationMailDtos.cs
@@ -0,0 +1,16 @@
+namespace backend.Dtos.InformationMailDto
+{
+    public class InformationMailDto
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string Transportation { get; set; }
+        public string Departure_Date { get; set; }
+        public string End_Date { get; set; }
+        public string Destination { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
